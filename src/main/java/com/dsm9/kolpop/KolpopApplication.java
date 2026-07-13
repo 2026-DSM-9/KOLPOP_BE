511773646b5fc3338239eccd52cc7deafa163ed4
spec3290 <spec3290@gmail.com>
2026-07-13 16:04:07 +0900

    chore: initialize project

---
+++ src/main/java/com/dsm9/kolpop/KolpopApplication.java
@@ -0,0 +1,13 @@
+package com.dsm9.kolpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolpopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolpopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsm9/kolpop/KolpopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm9.kolpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolpopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
